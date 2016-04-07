512d384e0579797c25fdd30f4b9327fa9cd26ff7
John Whittenburg <jawhittenburg@gmail.com>
2016-04-06 19:07:32 -0500

    can render blogs to ze page

--- app/routes/blog.js
+++ app/routes/blog.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return fetch(`https://tiny-tn.herokuapp.com/collections/blogs`)
-    .then((response) =>.json());
+    .then((response) => response.json());
   }
 });
